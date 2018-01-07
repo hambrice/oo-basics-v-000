7ffba344bd0d7e0ac6d1309b914f58ec297930ec
Brad Hambrice <hambrice@uab.edu>
2018-01-07 05:36:25 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,13 @@
 class Book
-  attr_accessor :title :author
+  attr_accessor :title
   def initialize(title)
     @title=title
+  end
+  def author=(author)
+    @author=author
+  end
+  def author
+    @author
   end
 end 
 
